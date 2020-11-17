99a52763e4046d40f178f5068879483f3e4f1d3f
GalDevel <64193128+GalDevel@users.noreply.github.com>
2020-11-17 14:25:18 +0200

    Added support for detailed version for Ubuntu (#1)

--- analyzer/os/ubuntu/ubuntu.go
+++ analyzer/os/ubuntu/ubuntu.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"os"
+	"regexp"
 	"strings"
 
 	"golang.org/x/xerrors"
@@ -22,25 +23,44 @@ var requiredFiles = []string{"etc/lsb-release"}
 
 type ubuntuOSAnalyzer struct{}
 
+var ubuntuVersionRegexp = regexp.MustCompile(`[\d.]+`)
+
 func (a ubuntuOSAnalyzer) Analyze(content []byte) (analyzer.AnalyzeReturn, error) {
 	isUbuntu := false
+	ubuntuVer, ubuntuDetailedVer := "", ""
+
 	scanner := bufio.NewScanner(bytes.NewBuffer(content))
 	for scanner.Scan() {
 		line := scanner.Text()
+
 		if line == "DISTRIB_ID=Ubuntu" {
 			isUbuntu = true
-			continue
 		}
-
-		if isUbuntu && strings.HasPrefix(line, "DISTRIB_RELEASE=") {
-			return analyzer.AnalyzeReturn{
-				OS: types.OS{
-					Family: aos.Ubuntu,
-					Name:   strings.TrimSpace(line[16:]),
-				},
-			}, nil
+		// Release contains only the release version of Ubuntu (e.g. 18.04)
+		// Since we want to more detailed version, dont overwrite if it was set
+		if ubuntuVer == "" && strings.HasPrefix(line, "DISTRIB_RELEASE=") {
+			ubuntuVer = strings.TrimSpace(line[16:])
+		}
+		// Description contains the detailed version of Ubuntu (e.g. 18.04.2)
+		if strings.HasPrefix(line, "DISTRIB_DESCRIPTION=") {
+			ubuntuDetailedVer = ubuntuVersionRegexp.FindString(line)
 		}
 	}
+
+	finalVer := ubuntuDetailedVer
+	if finalVer == "" {
+		finalVer = ubuntuVer
+	}
+
+	if isUbuntu {
+		return analyzer.AnalyzeReturn{
+			OS: types.OS{
+				Family: aos.Ubuntu,
+				Name:   finalVer,
+			},
+		}, nil
+	}
+
 	return analyzer.AnalyzeReturn{}, xerrors.Errorf("ubuntu: %w", aos.AnalyzeOSError)
 }
 
